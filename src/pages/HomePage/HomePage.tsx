825a44b98fa59e97911d86f1cf1e5175896025d3
korkeng <Chawanan.akk@mail.kmutt.ac.th>
2020-11-22 01:16:42 +0700

    Initialize Prettier to the Project

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import './HomePage.css';
 
 const App = () => {
-  return (
-    <div className="HomePage">
-      Hello World
-    </div>
-  );
-}
+  return <div className="HomePage"><div>Hello World</div></div>;
+};
 
 export default App;
