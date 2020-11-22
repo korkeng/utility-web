0aed2903ab61a36fcb04c3aa7b6c7fa032641a06
korkeng <Chawanan.akk@mail.kmutt.ac.th>
2020-11-22 12:42:13 +0700

    Remove unused import

--- src/components/NavigationBar.test.tsx
+++ src/components/NavigationBar.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import NavigationBar from './NavigationBar';
 
 test('renders learn react link', () => {
