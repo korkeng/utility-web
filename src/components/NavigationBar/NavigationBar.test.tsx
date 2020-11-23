84b302b6046d6165d16178a8bf2d6d5e9dc4242b
korkeng <Chawanan.akk@mail.kmutt.ac.th>
2020-11-23 23:49:57 +0700

    Remove test files and snapshot

--- src/components/NavigationBar/NavigationBar.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import NavigationBar from './NavigationBar';
-
-test('NavigationBar', () => {
-  const tree = render(<NavigationBar />);
-  expect(tree).toMatchSnapshot();
-});
--- src/pages/HomePage/HomePage.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import HomePage from './HomePage';
-
-test('HomePage', () => {
-  render(<HomePage />);
-  const linkElement = screen.getByText(/Hello World/i);
-  expect(linkElement).toBeInTheDocument();
-});
