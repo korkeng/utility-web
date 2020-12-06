3155ff5fa7e2a110473f6460b959db033431aaaa
korkeng <Chawanan.akk@mail.kmutt.ac.th>
2020-12-06 21:04:56 +0700

    Add language switch images

--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -1,5 +1,7 @@
 const Images = {
   icLogo: require('./ic_logo/logo.jpg'),
+  icLanguageEN: require('./ic_language/en.png'),
+  icLanguageTH: require('./ic_language/th.png'),
 };
 
 export default Images;
